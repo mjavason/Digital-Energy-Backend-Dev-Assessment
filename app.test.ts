46b006f0cb85389974639f5ea4dc07f644088e98
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2024-08-26 13:35:52 +0100

    Add tests

--- app.test.ts
+++ app.test.ts
@@ -1,7 +1,15 @@
-describe('App', () => {
-  describe('Demo', () => {
-    it('should just pass', async () => {
-      expect(1 + 1).toBe(2);
-    });
+import axios from 'axios';
+
+describe('External API - Site Live Test', () => {
+  it('should confirm the site is live and returns a successful response', async () => {
+    const url = `https://api.restful-api.dev`;
+
+    const response = await axios.get(url);
+
+    // Ensure the response status is 200 (OK)
+    expect(response.status).toBe(200);
+
+    // Optionally, check if the data exists in the response
+    expect(response.data).toBeDefined();
   });
 });
